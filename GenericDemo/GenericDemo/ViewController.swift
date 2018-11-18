19b8564ad7fa83b773a121b6b92376191b32d5bc
nguyentienhoang <nguyentienhoang810@gmail.com>
2018-11-18 12:34:06 +0700

    init project

---
+++ GenericDemo/GenericDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericDemo
+//
+//  Created by nguyentienhoang on 11/18/18.
+//  Copyright © 2018 nguyentienhoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
